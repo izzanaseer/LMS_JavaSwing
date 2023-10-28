381a5827e68c5f0c6da0c78807e416b71cf5979d
izzanaseer <izzanaseer3@gmail.com>
2023-10-28 16:41:11 +0500

    Version 2.2

---
+++ src/TableBtnEditor.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class TableBtnEditor extends DefaultCellEditor {
+    private final JButton button;
+    private String label;
+    private boolean isPushed;
+
+    public TableBtnEditor(JTextField textField)
+    {
+        super(textField);
+        button = new JButton();
+        button.setOpaque(true);
+        button.addActionListener((ActionEvent e) ->
+        {
+            fireEditingStopped();
+        });
+    }
+
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        if (isSelected) {
+            button.setForeground(table.getSelectionForeground());
+            button.setBackground(table.getSelectionBackground());
+        } else {
+            button.setForeground(table.getForeground());
+            button.setBackground(table.getBackground());
+        }
+        label = (value == null) ? "" : value.toString();
+        button.setText(label);
+        isPushed = true;
+        return button;
+    }
+
+        @Override
+        public Object getCellEditorValue()
+        {
+            if (isPushed)
+            {
+            }
+            isPushed = false;
+            return label;
+        }
+
+        @Override
+        public boolean stopCellEditing()
+        {
+            isPushed = false;
+            return super.stopCellEditing();
+        }
+}
---
+++ src/TableRenderBtn.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+
+public class TableRenderBtn extends JButton implements TableCellRenderer {
+    public TableRenderBtn()
+    {
+        setOpaque(true);
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        setText("read");
+        return this;
+    }
+}
